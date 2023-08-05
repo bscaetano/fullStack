8059e047797460e37bb0e7828dff5b3eda74e63a
bscaetano <bscaetano.teste@gmail.com>
2023-08-05 20:55:17 -0300

    trabalhando com Strings

---
+++ strv1.js
@@ -0,0 +1,17 @@
+var nome = " Marcelo da Silva ";
+var x = nome.length;
+console.log(x);
+
+resultado = x > 15 ? " Obrigado! " : " Por favor preencher seu nome completo ";
+
+console.log(resultado);
+
+var res = nome.toLocaleUpperCase();//tudo em maiuscula
+console.log(res);
+
+var jogo = " Barcelona vs Real ";
+var posicao = jogo.lastIndexOf("Real");//posicao de uma palavra
+console.log(posicao);
+
+var conrtado = jogo.slice(0,5);//corta a palavra. 0 começo 5 fim
+console.log(conrtado);
