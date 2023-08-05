d8103876fac2a16e9a41f21730352f38b1ce788a
bscaetano <bscaetano.teste@gmail.com>
2023-08-05 20:09:04 -0300

    Exercício calculo média ponderada de um aluno.

---
+++ calculomedia.js
@@ -0,0 +1,12 @@
+//Faca um algoritmo que leia tres notas de um aluno,
+//calcule e escreva a média final deste aluno.
+//Considerar que a media e ponderada e que o peso das notas e 2, 3, e 5.
+//Formula para o calculo da media final eh:
+
+var n1 = 5;
+var n2 = 4;
+var n3 = 7;
+var media = 0;
+
+media = (n1*2 + n2*3 + n3*5)/10;
+console.log(" A média ponderada é: " + media);
