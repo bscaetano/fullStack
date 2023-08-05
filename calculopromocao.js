3231e9a20454f7d9182560d9204494c49b7622af
bscaetano <bscaetano.teste@gmail.com>
2023-08-05 20:24:19 -0300

    Exercício produto com desconto (promoção).

---
+++ calculopromocao.js
@@ -0,0 +1,9 @@
+//Para fazer uma promocao, os comerciantes estao procurando aumentar suas venda
+//oferecendo desconto. Faca um algoritmo que possa
+//receber um valor de um produto e que escreva o novo
+//valor tendo em vista que o desconto foi de 9%.
+var valor = 1000;
+var valorDesconto = valor * 0.09;//referente a 9% de desconto
+var valorFinal = valor - valorDesconto;
+
+console.log(" Preço com desconto: " + valorFinal);
