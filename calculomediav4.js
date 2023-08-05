bff819a3c5ce7eae3a3bf44540ee2711115a6057
bscaetano <bscaetano.teste@gmail.com>
2023-08-05 20:17:54 -0300

    Exercício media ponderada 2

---
+++ calculomediav4.js
@@ -0,0 +1,12 @@
+//Faca um algoritmo que leia quatro numeros
+//informados pelo usuario e que depois imprima a
+//media ponderada, sabendo-se que os pesos sao
+//respectivamente: 1, 2, 3 e 4:
+var n1 = 5;
+var n2 = 4;
+var n3 = 7;
+var n4 = 8;
+var media = 0;
+
+media = (n1*1 + n2*2 + n3*3 + n4*4)/10;
+console.log(" A media ponderada é " + media);
