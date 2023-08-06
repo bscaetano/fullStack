bbc6566824f00067883894b96cfb19365efa93b7
bscaetano <bscaetano.teste@gmail.com>
2023-08-05 21:02:51 -0300

    atualização do exercício de Strings.

--- strv1.js
+++ strv1.js
@@ -15,3 +15,19 @@ console.log(posicao);
 
 var conrtado = jogo.slice(0,5);//corta a palavra. 0 começo 5 fim
 console.log(conrtado);
+
+var val = jogo.includes("Real");//encontrar palavra
+console.log(val);
+
+var str1 = " Hello ";
+var str2 = " Turma ";
+
+var str3 = str1.concat(str2);//concatenado Strings com concat()
+console.log(str3);
+
+var frase = "         Olá vc está aprensendo JavaScript ";
+console.log(frase.trim());//retira espaços
+
+var num = "0,1,2,3,4,5,6,7,8,9";
+var arr = num.split(",");//transforma em um array. A virgula é o separador, mas poderia ser | . etc...
+
