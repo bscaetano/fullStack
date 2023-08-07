a89de539e636a54f2c04dcf3d2b6ed15df1536dd
bscaetano <bscaetano.teste@gmail.com>
2023-08-06 22:38:39 -0300

    Exercício de métodos com array.

--- metodosArray.js
+++ metodosArray.js
@@ -13,9 +13,27 @@ console.log(arr1);
 console.log(" Posição do número 3: "+arr1.indexOf(3));
 //adiciona um item no array, neste caso, no final.
 arr1.push(7);
-console.log(arr1);
+console.log(" Adicionado valor no final "+arr1);
+//unshift adiciona no início
+arr1.unshift(0);
+console.log(" Adicionado valor no começo "+arr1);
 //retirando o último valor de um array
 arr1.pop();
 console.log(arr1);
+//removendo elementos com slice();
+var arr2 = [1,2,3,4,5,6,7];
+console.log(" Array antes da remoção com splice(): "+arr2);
+arr2.splice(1,1);//apartir da posição 1 retirar um elemento
+console.log(" Array depois da remoção de um elemento com splice(): "+arr2);
+//retirando mais de um elemento
+console.log(" Array antes da remoção com splice(): "+arr2);
+arr2.splice(1,2);//apartir da posição 1 retirar dois elementos
+console.log(" Array depois da remoção de dois elementos com splice(): "+arr2);
 
-var arr2 = [-5,10,23,-3,89];
+//substituindo valores de um array com splice()
+var nomes = ["Maria","João","Lucas","Pedro"];
+var novos = nomes.splice(1,1,"Luiz");//na posição 1 retira um valor e acrescentar nesta posição o nome Luiz, ficando o valor retirado armazenado em novos
+console.log(" O valor retirado foi: "+novos);
+console.log(" O valor do array agora é: "+nomes);
+//OBS.: pode ser retirado e substituido mais valores utilizando
+//var novos = nomes.splice(1,2,"nome1","nome2");
